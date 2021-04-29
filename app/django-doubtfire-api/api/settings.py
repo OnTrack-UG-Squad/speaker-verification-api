fc2ff87417962c483e66ea122b4b24bca21e6377
Nam Nguyen <nguyenvietnam2401@gmail.com>
2021-04-29 17:58:08 +1000

    config: remove unnecessary celery configuration option

--- app/django-doubtfire-api/api/settings.py
+++ app/django-doubtfire-api/api/settings.py
@@ -122,9 +122,6 @@ CACHES = {
 # https://docs.celeryproject.org/en/stable/userguide/configuration.html
 CELERY_BROKER_URL = REDIS_URL
 CELERY_RESULT_BACKEND = REDIS_URL
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
 
 
 # Internationalization
