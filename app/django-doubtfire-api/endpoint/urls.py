de28cb4db86da4accfd071bff7691147de2d42f7
Nam Nguyen <nguyenvietnam2401@gmail.com>
2021-05-05 17:28:27 +1000

    feat: add path to redirect to Flower dashboard

--- app/django-doubtfire-api/endpoint/urls.py
+++ app/django-doubtfire-api/endpoint/urls.py
@@ -1,9 +1,15 @@
 from django.urls import include, path
 from django.conf.urls import url
-from endpoint.views import enroll_user, validate_recording, redis_healthcheck
+from endpoint.views import (
+    enroll_user,
+    validate_recording,
+    check_redis_health,
+    redirect_flower_dashboard,
+)
 
 urlpatterns = [
     path("enroll", enroll_user),
     path("validate", validate_recording),
-    path("redis-healthcheck", redis_healthcheck, name="up"),
+    path("redis-healthcheck", check_redis_health, name="up"),
+    path("flower", redirect_flower_dashboard),
 ]
